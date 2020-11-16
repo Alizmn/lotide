61c046e0c0a83b9c60a939e86d8a2bf0e52414e1
Ali Zamani <Ali.zmn@hotmail.com>
2020-11-16 22:44:11 +0000

    Adding templates

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-  (actual === expected) ? (console.log("✅ Assertion Passed: [" + actual + "] === [" + expected + "]")) : (console.log("🛑 Assertion Failed: [" + actual + "] !== [" + expected + "]"));
+  (actual === expected) ? (console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`)) : (console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`));
 };
 assertEqual("Ali", 1);
 assertEqual(1, 1);
